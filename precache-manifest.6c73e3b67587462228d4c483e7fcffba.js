3f51ce478ebf28ba3e560cb07f153bc70ea629d6
Parthkumar Dave <Parth.Dave@civica2.com>
2020-09-05 23:44:29 +0530

    Updates

---
+++ precache-manifest.6c73e3b67587462228d4c483e7fcffba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb97ebcc4d589cebf95ced5226c380c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "adb8d068b9943e5b1a57",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9e7d9665e309ffbb5a5",
+    "url": "./static/js/2.a6dfc554.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.a6dfc554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb8d068b9943e5b1a57",
+    "url": "./static/js/main.b203082a.chunk.js"
+  },
+  {
+    "revision": "f0d61d92ee430047a83d",
+    "url": "./static/js/runtime-main.b28d2f65.js"
+  }
+]);
